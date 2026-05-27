fe0995ba68339d725c6b0cdb02aff756a5bf5fd9
rahimkominfo <rahimkominfo@gmail.com>
2026-05-28 07:36:51 +0800

    perbaikan

--- app/Views/frontend/home.php
+++ app/Views/frontend/home.php
@@ -294,7 +294,7 @@
             <div class="relative w-full aspect-video rounded-3xl overflow-hidden shadow-2xl border-4 border-green-50/50 group">
                 <iframe 
                     class="w-full h-full" 
-                    src="https://youtube.com/embed/rITN3eWU5Ho&t" 
+                    src="https://youtube.com/embed/rITN3eWU5Ho" 
                     title="YouTube video player" 
                     frameborder="0" 
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
